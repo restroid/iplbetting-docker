4eaf33988775076e2ed22e626aba3dc95f352f89
Ritesh Kumar <ritesh@flick2know.com>
2019-06-25 17:52:38 +0530

    cheges

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -7,7 +7,7 @@ fi
 
 domains=(ipl.restroid.in)
 rsa_key_size=4096
-data_path="."
+data_path="./data/certbot"
 email="riteshbxr@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
